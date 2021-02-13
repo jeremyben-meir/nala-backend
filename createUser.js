a6f90b73ac11416306c52d968bdabd81b773a3b5
Jeremy Ben-Meir <jsb459@cornell.edu>
2021-02-13 17:43:55 -0500

    Adding our Serverless API

--- createUser.js
+++ createUser.js
@@ -1,15 +1,13 @@
-import * as uuid from "uuid";
+// import * as uuid from "uuid";
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
-
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export const main = handler(async (event, context) => {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
   const params = {
-    TableName: process.env.tableName,
+    TableName: process.env.userTableName,
     Item: {
       userId: "123", // The id of the author
       firstName: data.firstName, // A unique uuid
