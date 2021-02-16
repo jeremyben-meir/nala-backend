90c5cb184893e50346a3f9cdaa82863b56000d3d
Jeremy Ben-Meir <jsb459@cornell.edu>
2021-02-16 16:38:15 -0500

    userID fix

--- createUser.js
+++ createUser.js
@@ -6,10 +6,16 @@ export const main = handler(async (event, context) => {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
+  const authProvider = event.requestContext.identity.cognitoAuthenticationProvider;
+  const parts = authProvider.split(':');
+  // const userPoolIdParts = parts[parts.length - 3].split('/');
+  // const userPoolId = userPoolIdParts[userPoolIdParts.length - 1];
+  const userPoolUserId = parts[parts.length - 1];
+
   const params = {
     TableName: process.env.userTableName,
     Item: {
-      userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
+      userId: userPoolUserId, // The id of the author
       firstName: data.firstName, // A unique uuid
       lastName: data.lastName, // Parsed from request body
       profImg: data.profImg, // Parsed from request body
